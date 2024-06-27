af5e0e1a9d3e2b29bd0f97313f5aa4a13ac969b8
G0SU19O2 <huyquocnguyen1902@gmail.com>
2024-06-27 15:00:40 +0700

    Request data from URL `load`

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -9,17 +9,28 @@ import Foundation
 import Testing
 
 class RemoteFeedLoader {
-    
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "https://google.com")
+    }
 }
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    private init() {}
     var requestedURL: URL?
 }
 
 struct RemoteFeedLoaderTests {
     @Test func init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         _ = RemoteFeedLoader()
         #expect(client.requestedURL == nil)
+    }
+
+    @Test func load_requestDataFromURL() {
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+        sut.load()
+        #expect(client.requestedURL != nil)
     }
 }
