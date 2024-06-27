7baaccb7a4a37c60c6403ae9e4b84ddd3c26b62f
G0SU19O2 <huy.nguyen15@sotatek.com>
2024-06-27 14:23:40 +0700

    Create EssentialFeed framework

---
+++ EssentialFeedTests/EssentialFeedTests.swift
@@ -0,0 +1,17 @@
+//
+//  EssentialFeedTests.swift
+//  EssentialFeedTests
+//
+//  Created by Quốc Huy Nguyễn on 6/27/24.
+//
+
+import Testing
+@testable import EssentialFeed
+
+struct EssentialFeedTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
