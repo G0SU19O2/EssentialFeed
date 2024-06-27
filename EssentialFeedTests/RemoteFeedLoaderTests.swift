4218957fc01da78fe1f53927892c57bd217bf3d2
G0SU19O2 <huyquocnguyen1902@gmail.com>
2024-06-27 16:21:47 +0700

    Simplify spy

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -11,14 +11,14 @@ import Testing
 struct RemoteFeedLoaderTests {
     @Test func init_doesNotRequestDataFromURL() {
         let (_, client) = makeSUT()
-        #expect(client.requestedURL == nil)
+        #expect(client.requestedURLs.isEmpty)
     }
 
     @Test func load_requestsDataFromURL() {
         let url = URL(string: "https://google.com")!
         let (sut, client) = makeSUT(url: url)
         sut.load()
-        #expect(client.requestedURL == url)
+        #expect(client.requestedURLs == [url])
     }
     
     @Test func loadTwice_requestDataFromURL() {
@@ -37,10 +37,8 @@ struct RemoteFeedLoaderTests {
     }
 
     class HTTPClientSpy: HTTPClient {
-        var requestedURL: URL?
         var requestedURLs: [URL] = []
         func get(from url: URL) {
-            requestedURL = url
             requestedURLs.append(url)
         }
     }
