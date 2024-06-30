2d2c9dd323900816f699549f61b5d700130b9ff3
G0SU19O2 <huyquocnguyen1902@gmail.com>
2024-06-30 12:59:49 +0700

    Move mapping logic to a new type `FeedItemsMapper`

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -28,9 +28,10 @@ public final class RemoteFeedLoader {
         client.get(from: url) { result in
             switch result {
             case let .success((data, response)):
-                if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
-                    completion(.success(root.items.map { $0.item }))
-                } else {
+                do {
+                    let item = try FeedItemMapper.map(data, from: response)
+                    completion(.success(item))
+                } catch {
                     completion(.failure(.invalidData))
                 }
             case .failure:
@@ -44,17 +45,26 @@ public protocol HTTPClient {
     func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
 }
 
-private struct Root: Decodable {
-    let items: [Item]
-}
+private class FeedItemMapper {
+    private struct Root: Decodable {
+        let items: [Item]
+    }
 
-private struct Item: Decodable {
-    let id: UUID
-    let description: String?
-    let location: String?
-    let image: URL
+    private struct Item: Decodable {
+        let id: UUID
+        let description: String?
+        let location: String?
+        let image: URL
 
-    var item: FeedItem {
-        FeedItem(id: id, description: description, location: location, imageURL: image)
+        var item: FeedItem {
+            FeedItem(id: id, description: description, location: location, imageURL: image)
+        }
+    }
+
+    static func map(_ data: Data, from response: HTTPURLResponse) throws -> [FeedItem] {
+        guard response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
+            throw RemoteFeedLoader.Error.invalidData
+        }
+        return root.items.map { $0.item }
     }
 }
