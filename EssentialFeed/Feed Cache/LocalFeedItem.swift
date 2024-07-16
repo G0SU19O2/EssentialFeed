03840b1d1ebc98d2fe6bdde3dec15806dd2b81a5
G0SU19O2 <huyquocnguyen1902@gmail.com>
2024-07-16 16:54:17 +0700

    Move LocalFeedItem to its own file

---
+++ EssentialFeed/Feed Cache/LocalFeedItem.swift
@@ -0,0 +1,22 @@
+//
+//  LocalFeedItem.swift
+//  EssentialFeed
+//
+//  Created by Quốc Huy Nguyễn on 7/16/24.
+//
+
+import Foundation
+
+public struct LocalFeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
