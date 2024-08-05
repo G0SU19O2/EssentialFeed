2f6aadbffcde81475575f60ffeb566fb9a94792f
G0SU19O2 <huyquocnguyen1902@gmail.com>
2024-08-05 15:41:58 +0700

    CoreDataFeedStore.deleteCachedFeed() delivers no error on non-empty cache

--- EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -54,7 +54,10 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
         assertThatDeleteHasNoSideEffectsOnEmptyCache(on: sut)
     }
     
-    func test_delete_deliversNoErrorOnNonEmptyCache() {}
+    func test_delete_deliversNoErrorOnNonEmptyCache() {
+        let sut = makeSUT()
+        assertThatDeleteDeliversNoErrorOnNonEmptyCache(on: sut)
+    }
     
     func test_delete_emptiesPreviouslyInsertedCache() {}
     
